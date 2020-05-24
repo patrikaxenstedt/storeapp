f361c91606368e3e7bdf1d0c7c27a3521c847da4
patrikaxenstedt <patrik.eriksson.axenstedt@chasacademy.se>
2020-05-24 12:56:51 +0200

    add database

--- CreateDataBase.sql
+++ CreateDataBase.sql
@@ -2,10 +2,12 @@ DROP DATABASE IF EXISTS yellowstore;
 CREATE DATABASE yellowstore;
 USE yellowstore;
 
+CREATE SEQUENCE ships_seq;
+
 CREATE TABLE ships
 (
     id INT NOT NULL
-    AUTO_INCREMENT,
+        DEFAULT NEXTVAL ('ships_seq'),
     shipName CHAR
     (100) NOT NULL,
     shipOrigin VARCHAR
@@ -17,25 +19,25 @@ CREATE TABLE ships
     PRIMARY KEY
     (id)
 );
-    INSERT INTO ships
-        (shipName, shipOrigin, shipClass, shipPrice)
-    VALUES
-        ('Enteprise-1', 'Startrek', 'Galaxy', '666'),
-        ('Enterprise-2', 'Startrek', 'Sovreign', '666'),
-        ('Star-Destroyer', 'Starwars', 'Battleship', '800'),
-        ('Imperial-Landing-Craft', 'Starwars', 'Shuttle', '444'),
-        ('U.S.S-Sulaco', 'Aliens', 'Light-Assault-Carrier', '600'),
-        ('The-Nostromo', 'Aliens', 'Hauler', '500'),
-        ('Rocinante', 'The-Expanse', 'Corvette', '800'),
-        ('SSV-Normandy', 'MassEffect', 'Frigate', '200'),
-        ('USCSS-Prometheus', 'Prometheus', 'Heliades', '1000'),
-        ('Millennium-Falcon', 'Starwars', 'Battleship', '700'),
-        ('X-wing', 'Starwars', 'Battleship', '700');
+INSERT INTO ships
+    (shipName, shipOrigin, shipClass, shipPrice)
+VALUES
+    ('Enteprise-1', 'Startrek', 'Galaxy', '666'),
+    ('Enterprise-2', 'Startrek', 'Sovreign', '666'),
+    ('Star-Destroyer', 'Starwars', 'Battleship', '800'),
+    ('Imperial-Landing-Craft', 'Starwars', 'Shuttle', '444'),
+    ('U.S.S-Sulaco', 'Aliens', 'Light-Assault-Carrier', '600'),
+    ('The-Nostromo', 'Aliens', 'Hauler', '500'),
+    ('Rocinante', 'The-Expanse', 'Corvette', '800'),
+    ('SSV-Normandy', 'MassEffect', 'Frigate', '200'),
+    ('USCSS-Prometheus', 'Prometheus', 'Heliades', '1000'),
+    ('Millennium-Falcon', 'Starwars', 'Battleship', '700'),
+    ('X-wing', 'Starwars', 'Battleship', '700');
 
 
-    CREATE TABLE Buy
-    (
-        id INT,
-        shipAmmount CHAR(100),
-        FOREIGN KEY (id) REFERENCES ships(id)
-    );
+CREATE TABLE Buy
+(
+    id INT,
+    shipAmmount CHAR(100),
+    FOREIGN KEY (id) REFERENCES ships(id)
+);
